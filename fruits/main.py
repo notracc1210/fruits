1f79ad1cf0aea18d691dc1a4050e17947dae1ee1
notracc1210 <1210tracychen@gmail.com>
2026-01-13 10:42:20 -0500

    ask user for favorite fruit

--- fruits/main.py
+++ fruits/main.py
@@ -2,3 +2,8 @@ print("hello")
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
+user_input = input("Enter your favorite fruit: ")
+if user_input in fruits:
+    print("You chose a popular fruit!")
+else:
+    print("Sorry, that's not on the list. There are only: " + ", ".join(fruits))
